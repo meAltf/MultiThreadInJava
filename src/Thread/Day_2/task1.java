1e17fa5fb99f811e7229f2dca578e11ae2cf4904
meAltf <alataf.dce.cs1@gmail.com>
2025-06-26 01:52:58 +0530

    Task-2 completed

--- src/Thread/Day_2/task1.java
+++ src/Thread/Day_2/task1.java
@@ -17,11 +17,32 @@ class MyThreadTask1 extends Thread {
     }
 }
 
+/**
+ * Task 2: Create a class that implements Runnable
+ * It should print numbers from 1 to 10.
+ * Run it using a new thread.
+ */
+class MyThreadTask2 implements Runnable{
+    @Override
+    public void run() {
+        for(int i=1; i<=10; i++){
+            System.out.print(i +" ");
+        }
+    }
+}
+
 public class task1 {
     public static void main(String[] args) {
         System.out.println("Thread from main:-- " + Thread.currentThread().getName());
 
+        /* Task-1 */
         Thread threadTask1 = new MyThreadTask1();
         threadTask1.start();
+
+        /* Task-2 */
+        MyThreadTask2 threadTask2 = new MyThreadTask2();
+        Thread createNewThreadTask2 = new Thread(threadTask2);
+        createNewThreadTask2.start();
+
     }
 }
