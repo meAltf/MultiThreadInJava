e1e2bd63a9b8e024b69d28954ba3e9057305c878
meAltf <alataf.dce.cs1@gmail.com>
2025-06-26 02:04:31 +0530

    Task-3 completed

--- src/Thread/Day_2/task1.java
+++ src/Thread/Day_2/task1.java
@@ -1,5 +1,7 @@
 package Thread.Day_2;
 
+import java.time.LocalTime;
+
 /**
  * Task 1: Create a class that extends Thread
  * It should print "👋 Hello from [thread name]" five times with a 1-second pause.
@@ -31,6 +33,12 @@ class MyThreadTask2 implements Runnable{
     }
 }
 
+/**
+ Task 3: Use Lambda expression with Runnable
+ Print current time 3 times in a thread using lambda + Thread.sleep(1000).
+ */
+// check directly main function for this task-3
+
 public class task1 {
     public static void main(String[] args) {
         System.out.println("Thread from main:-- " + Thread.currentThread().getName());
@@ -43,6 +51,19 @@ public class task1 {
         MyThreadTask2 threadTask2 = new MyThreadTask2();
         Thread createNewThreadTask2 = new Thread(threadTask2);
         createNewThreadTask2.start();
+
+        /* Task-3 */
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Current Time : " + LocalTime.now());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+        });
+        thread.start();
 
     }
 }
