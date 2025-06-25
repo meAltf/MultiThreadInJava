d37a68ff4fab3c12fece908ce2cc70002d62eb8b
meAltf <alataf.dce.cs1@gmail.com>
2025-06-26 01:12:06 +0530

    creating thread by extending Thread

---
+++ src/Thread/ThreadExample.java
@@ -0,0 +1,20 @@
+package Thread;
+
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Running from the thread: " + Thread.currentThread().getName());
+    }
+}
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        System.out.println("Running from main Thread: " + Thread.currentThread().getName());
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+}
+
+/**
+ * 1. start() launches a new thread, which then calls run() internally.
+ * 2. Don't call run() directly—it won't start a new thread.
+ */
