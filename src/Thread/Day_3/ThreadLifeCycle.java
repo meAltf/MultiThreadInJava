58257a3c2dbeede2ad894381ba921d80cf105580
meAltf <alataf.dce.cs1@gmail.com>
2025-06-29 09:57:03 +0530

    Sleep example

--- src/Thread/Day_3/ThreadLifeCycle.java
+++ src/Thread/Day_3/ThreadLifeCycle.java
@@ -1,4 +1,15 @@
 package Thread.Day_3;
 
 public class ThreadLifeCycle {
+
+    public static void main(String[] args) {
+        System.out.println("Start");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException exception) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("End after 2 seconds");
+    }
 }
