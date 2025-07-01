b22596a3a701f5d8e3c0f2a246b6eac71811737f
meAltf <alataf.dce.cs1@gmail.com>
2025-07-02 00:04:19 +0530

    Thread priority demo

--- src/Thread/Day_4/PriorityDemo.java
+++ src/Thread/Day_4/PriorityDemo.java
@@ -1,4 +1,20 @@
 package Thread.Day_4;
 
 public class PriorityDemo {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(() ->
+                System.out.println("Thread-1 running!"));
+        Thread thread2 = new Thread(() ->
+                System.out.println("Thread-2 running!"));
+
+        thread1.setPriority(Thread.MIN_PRIORITY);
+        thread2.setPriority(Thread.MAX_PRIORITY);
+
+        System.out.println("Thread-1 priority " + thread1.getPriority());
+        System.out.println("Thread-2 priority " + thread2.getPriority());
+
+        thread1.start();
+        thread2.start();
+    }
 }
